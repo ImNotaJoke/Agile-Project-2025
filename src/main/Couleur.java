7d1be5fd864bab53add8f75b6519aae6c91bc127
Nathan Philippe <nathan.philippe.etu@univ-lille.fr>
2025-09-01 17:18:50 +0200

    Changement Couleur

--- src/main/Couleur.java
+++ src/main/Couleur.java
@@ -1,5 +1,39 @@
 package main;
 
+import java.util.Scanner;
+
 public enum Couleur {
-    BLANC, NOIR
+    BLANC, NOIR;
+
+    public static Couleur demandeCouleur(){
+        Scanner sc = new Scanner(System.in);
+        int choix = 0;
+
+        while (choix != 1 && choix != 2) {
+            System.out.println("Choisissez votre couleur :");
+            System.out.println("1 - Blanc");
+            System.out.println("2 - Noir");
+            System.out.print("Votre choix : ");
+            
+            if (sc.hasNextInt()) {
+                choix = sc.nextInt();
+            } else {
+                sc.next();
+            }
+
+            if (choix != 1 && choix != 2) {
+                System.out.println("Choix invalide, veuillez entrer 1 ou 2.");
+            }
+        }
+
+        Couleur couleurChoisie;
+
+        if(choix == 1){
+            couleurChoisie = Couleur.BLANC;
+        }
+        else{
+            couleurChoisie = Couleur.NOIR;
+        }
+        return couleurChoisie;
+    }
 }
