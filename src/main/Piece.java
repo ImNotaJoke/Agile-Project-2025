ec45aaf4abdcdacd2c8e987754777cefa0cce862
Camille <timothee.dupont.etu@univ-lille.fr>
2025-09-01 16:13:17 +0200

    update-piece

--- src/main/Piece.java
+++ src/main/Piece.java
@@ -3,4 +3,29 @@ package main;
 public class Piece{
     String color;
     PieceName name;
+
+    public String getColor(){
+        return this.color; 
+    }
+
+    public String getPiece(){
+        if(this.name == PieceName.PION){
+            return "P"; 
+        }
+        else if(this.name == PieceName.TOUR){
+            return "T"; 
+        }
+        else if(this.name == PieceName.CAVALIER){
+            return "C"; 
+        }
+        else if(this.name == PieceName.FOU){
+            return "F"; 
+        }
+        else if(this.name == PieceName.DAME){
+            return "D"; 
+        }
+        else{
+            return "R"; 
+        }
+    }
 }
