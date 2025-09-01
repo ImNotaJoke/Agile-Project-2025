822bada41f4dffec62acd072f38416f7766f7f80
Mathéo Lelong <matheo.lelong.etu@univ-lille.fr>
2025-09-01 15:43:59 +0200

    plateau

---
+++ src/main/Plateau.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Plateau {
+    private char[][] plateau;
+    private char[][] plateauCouleur;
+
+    public Plateau(){
+        plateau = new char[8][8];
+    }
+}
