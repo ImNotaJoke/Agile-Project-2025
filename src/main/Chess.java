f3c330399d30b3b5a11e6eb53b6ddf2fd055eef2
Sulivan Cerdan <sulivan.cerdan.etu@univ-lille.fr>
2025-09-02 11:17:39 +0200

    V1.5 sprint 3

---
+++ src/main/Chess.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Chess{
+    public static void main(String[] args) {
+        Plateau plateau = new Plateau();
+
+        plateau.getPlateau()[1][0].move(plateau, new int[] { 1, 0 }, new int[] { 2, 0 });
+        System.out.println(plateau);
+    }
+}
--- src/main/ChessWithMe.java
+++ src/main/ChessWithMe.java
@@ -7,18 +7,22 @@ import main.piece.Piece;
 
 public class ChessWithMe {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        // Joueur joueur1;
-        // Joueur joueur2;
-=======
-        Joueur joueur1;
-        Joueur joueur2;
-        String nom;
-        nom = "ChessWithMe";
-
-        joueur1 = new Joueur("Joueur 1");
-        joueur2 = new Joueur("Joueur 2");
->>>>>>> f0cea11d0a4921c83ea0e72e22302b5ee198115b
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Joueur 1 : entrez votre pseudo : ");
+        String nom = sc.nextLine(); 
+        Couleur couleur = Couleur.demandeCouleur();
+        Joueur j1 = new Joueur(nom,couleur); 
+        Scanner sc2 = new Scanner(System.in); 
+        System.out.println("Joueur 2 : entrez votre pseudo : ");
+        String nom2 = sc2.nextLine(); 
+        Joueur j2 = new Joueur(nom2); 
+        if ( j1.getCouleur() == Couleur.BLANC){
+            j2.setCouleur(Couleur.NOIR); 
+        }else{
+            j2.setCouleur(Couleur.BLANC); 
+        }
+        System.out.println("Joueur 1 : " + j1.getPseudo() + " (" + j1.getCouleur() + ")");
+        System.out.println("Joueur 2 : " + j2.getPseudo() + " (" + j2.getCouleur() + ")");
 
         Plateau plateau = new Plateau();
 
@@ -29,42 +33,6 @@ public class ChessWithMe {
         plateau.getPlateau()[0][0].move(plateau, new int[] { 0, 0 }, new int[] { 2, 0 });
         System.out.println(plateau);
 
-        // System.out.println(nom + "\n");
-        // Couleur couleur = Couleur.demandeCouleur();
-        // joueur1.setCouleur(couleur);
-        // if (couleur == Couleur.NOIR) {
-        //     joueur2.setCouleur(Couleur.BLANC);
-        // } else {
-        //     joueur2.setCouleur(Couleur.NOIR);
-        // }
-
-        Scanner sc = new Scanner(System.in); 
-        System.out.println("Joueur 1 : entrez votre pseudo : ");
-        String nom = sc.nextLine(); 
-        Couleur couleur = Couleur.demandeCouleur();
-        Joueur j1 = new Joueur(nom,couleur); 
-        Scanner sc2 = new Scanner(System.in); 
-        System.out.println("Joueur 2 : entrez votre pseudo : ");
-        String nom2 = sc2.nextLine(); 
-        Joueur j2 ; 
-        if ( j1.getCouleur == Couleur.BLANC)
-
-
-
     }
-
-    // public void choixPseudo() {
-
-    //     Joueur joueur1;
-    //     Joueur joueur2;
-    //     Scanner rep = new Scanner(System.in);
-    //     System.out.println(" Joueur 1 :Entrez votre pseudo: ");
-    //     String nom = rep.nextLine();
-    //     joueur1 = new Joueur(nom);
-    //     Scanner rep2 = new Scanner(System.in);
-    //     System.out.println("Joueur 2 : Entrez votre pseudo: ");
-    //     String nomj2 = rep2.nextLine();
-    //     joueur2 = new Joueur(nomj2);
-    // }
 
 }
