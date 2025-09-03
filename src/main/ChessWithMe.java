d22802f414712e4659273c1814bd4555a86ce36f
Nathan Philippe <nathan.philippe.etu@univ-lille.fr>
2025-09-03 13:52:20 +0200

    debug couleur

--- src/main/ChessWithMe.java
+++ src/main/ChessWithMe.java
@@ -40,6 +40,7 @@ public class ChessWithMe {
 
         Joueur blanc;
         Joueur noir;
+
         if ( j1.getCouleur() == Couleur.BLANC){
             blanc = j1;
             noir = j2;
@@ -50,14 +51,14 @@ public class ChessWithMe {
 
         do{
             do {
-                j1.echecJoueur(plateau);
-                int [] co1 = j1.choixDeplacement(j1.getPseudo() + " choisie une pièce à déplacer (ex: a2 ou 2a): ");
+                blanc.echecJoueur(plateau);
+                int [] co1 = blanc.choixDeplacement(blanc.getPseudo() + " choisie une pièce à déplacer (ex: a2 ou 2a): ");
                 if(co1[0] == -1 && co1[1] == -1){
                     start(type);
                     return;
                 }
-                int [] co2 = j2.choixDeplacement(j1.getPseudo() + " où voux tu bouger la pièce ? (ex: a2 ou 2a): ");
-                readyj1 = !j1.demanderDeplacement(plateau,co1,co2);
+                int [] co2 = blanc.choixDeplacement(blanc.getPseudo() + " où voux tu bouger la pièce ? (ex: a2 ou 2a): ");
+                readyj1 = !blanc.demanderDeplacement(plateau,co1,co2);
                 clearConsole();
                 System.out.println(plateau);
             } while (readyj1);
@@ -65,14 +66,14 @@ public class ChessWithMe {
             wait(1000);
 
             do {
-                j2.echecJoueur(plateau);
-                int [] co3 = j1.choixDeplacement(j2.getPseudo() + " choisie une pièce à déplacer (ex: a2 ou 2a): ");
+                noir.echecJoueur(plateau);
+                int [] co3 = noir.choixDeplacement(noir.getPseudo() + " choisie une pièce à déplacer (ex: a2 ou 2a): ");
                 if(co3[0] == -1 && co3[1] == -1){
                     start(type);
                     return;
                 }
-                int [] co4 = j2.choixDeplacement(j2.getPseudo() + " où voux tu bouger la pièce ? (ex: a2 ou 2a): ");
-                readyj2 = !j2.demanderDeplacement(plateau,co3,co4);
+                int [] co4 = noir.choixDeplacement(noir.getPseudo() + " où voux tu bouger la pièce ? (ex: a2 ou 2a): ");
+                readyj2 = !noir.demanderDeplacement(plateau,co3,co4);
                 clearConsole();
                 System.out.println(plateau);
             } while (readyj2);
