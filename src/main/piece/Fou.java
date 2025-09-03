6db696124643ed586f4e6fc0f793a87d8ef2fcc6
Camille <timothee.dupont.etu@univ-lille.fr>
2025-09-03 14:13:38 +0200

    patch

--- src/main/piece/Fou.java
+++ src/main/piece/Fou.java
@@ -22,10 +22,15 @@ public class Fou extends Piece {
 
     public boolean move(Plateau plateau, int[] oldPosition, int[] newPosition) {
         Piece[][] plat = plateau.getPlateau();
-        if (moveIsOk(plateau.getPlateau(), oldPosition, newPosition)  && (plateau.getRoi(color).echec(plat, plateau.getPositionRoi(this.getColor())) == false)) {
+        if (moveIsOk(plateau.getPlateau(), oldPosition, newPosition)) {
             plateau.getPlateau()[newPosition[0]][newPosition[1]] = this;
-            plateau.getPlateau()[oldPosition[0]][oldPosition[1]] = null;
-            return true;
+            if((plateau.getRoi(color).echec(plat, plateau.getPositionRoi(this.getColor())) == false) || plateau.getRoi(color).echecPos(plat, plateau.getPositionRoi(this.getColor())) == newPosition) {
+                plateau.getPlateau()[oldPosition[0]][oldPosition[1]] = null;
+                return true;
+            }else {
+                plateau.getPlateau()[oldPosition[0]][oldPosition[1]] = this;
+                plateau.getPlateau()[newPosition[0]][newPosition[1]] = null;
+            }
         }
         System.out.println("Mouvement impossible pour le fou");
         return false;
