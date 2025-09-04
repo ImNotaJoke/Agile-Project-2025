d2c582e2cdf9b44287f66e60757df99d7ff15387
Sulivan Cerdan <sulivan.cerdan.etu@univ-lille.fr>
2025-09-04 09:02:42 +0200

    fix bot

---
+++ test/ChessWitMeTest.java
@@ -0,0 +1,28 @@
+package test;
+import static org.junit.jupiter.api.Assertions.*;
+import main.Plateau;
+import main.Demo;
+import main.Bot;
+import main.piece.*;
+import main.Couleur;
+
+
+public class ChessWitMeTest {
+    
+    @org.junit.jupiter.api.Test
+    public void testInitialisationPlateau() {
+
+        Plateau plateau = new Plateau(Demo.GAME);
+        assertNotNull(plateau);
+        assertEquals(8, plateau.getPlateau().length);
+        assertEquals(8, plateau.getPlateau()[0].length);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testInitialisationBot() {
+        Bot bot = new Bot(Couleur.NOIR);
+        assertNotNull(bot);
+        assertEquals(Couleur.NOIR, bot.getCouleur());
+    }
+
+}
